e27825455f4f0c44a7f4505a85ae92da59e1e634
josedurazoc <josedurazoc@gmail.com>
2017-05-16 14:05:23 -0700

    Modelos creados por JOSE Se anexan los modelos de PC , SALA Y USUARIO

---
+++ src/com/cahd/bitacora/models/models/Sala.java
@@ -0,0 +1,57 @@
+package com.cahd.bitacora.models.models;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Sala {
+    private final StringProperty edificio;
+    private final IntegerProperty noSala;
+    private final StringProperty nombreSala;
+
+    public Sala(){
+        this(null,null,null);
+    }
+    public Sala(String edificio, Integer noSala, String nombreSala) {
+        this.edificio = new SimpleStringProperty(edificio);
+        this.noSala = new SimpleIntegerProperty(noSala);
+        this.nombreSala = new SimpleStringProperty(nombreSala);
+    }
+
+    public String getEdificio() {
+        return edificio.get();
+    }
+
+    public StringProperty edificioProperty() {
+        return edificio;
+    }
+
+    public void setEdificio(String edificio) {
+        this.edificio.set(edificio);
+    }
+
+    public int getNoSala() {
+        return noSala.get();
+    }
+
+    public IntegerProperty noSalaProperty() {
+        return noSala;
+    }
+
+    public void setNoSala(int noSala) {
+        this.noSala.set(noSala);
+    }
+
+    public String getNombreSala() {
+        return nombreSala.get();
+    }
+
+    public StringProperty nombreSalaProperty() {
+        return nombreSala;
+    }
+
+    public void setNombreSala(String nombreSala) {
+        this.nombreSala.set(nombreSala);
+    }
+}
