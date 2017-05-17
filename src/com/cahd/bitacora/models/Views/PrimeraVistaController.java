e23ea1d604eb9dfc90c894110cc283959bd5216e
cmbarcelo <cmbarcelo96@gmail.com>
2017-05-17 11:43:19 -0800

    Controlador de prestamoSala
    espero y les guste

---
+++ src/com/cahd/bitacora/models/Views/PrimeraVistaController.java
@@ -0,0 +1,25 @@
+package com.cahd.bitacora.models.Views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by Marina on 09/05/2017.
+ */
+public class PrimeraVistaController {
+    @FXML
+    private TextField txtMatricula;
+
+
+    @FXML
+    public void iniciar(){
+        int matricula=0;
+        matricula=Integer.parseInt(txtMatricula.getText());
+
+
+        /*
+         Aqui se validara la matricula y dependiendo de cual sea ya sea de Maestro, alumno, administrador
+         sera la vista que se mostrara en la siguiente pantalla.
+        */
+    }
+}
